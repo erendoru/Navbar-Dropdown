0925fc44c34a2127304045ad881bf96b140a3720
Eren Doru <erendoru@hotmail.com>
2020-10-08 21:47:06 +0300

    Navbar Dropdown
    Every Components at once.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Services from "./components/pages/Services";
+import Home from "./components/pages/Home";
+import Marketing from "./components/pages/Marketing";
+import Consulting from "./components/pages/Consulting";
+import ContactUs from "./components/pages/ContactUs";
+
+import SignUp from "./components/pages/SignUp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/services" exact component={Services} />
+        <Route path="/contact-us" exact component={ContactUs} />
+        <Route path="/sign-up" exact component={SignUp} />
+        <Route path="/marketing" exact component={Marketing} />
+        <Route path="/consulting" exact component={Consulting} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/MenuItems.js
@@ -0,0 +1,24 @@
+const MenuItems = [
+  {
+    title: "Services",
+    path: "/services",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Consulting",
+    path: "/consulting",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Marketing",
+    path: "/marketing",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Sign Up",
+    path: "/sign-up",
+    cName: "dropdown-link",
+  },
+];
+
+export default MenuItems;
